4cc33c86b423f0e322c1dec26713bfa62cdafff0
vasilij korobov <kavyabass51@gmail.com>
2025-05-07 23:32:03 +0700

    Задание 1

--- materials/serializers.py
+++ materials/serializers.py
@@ -10,7 +10,11 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
