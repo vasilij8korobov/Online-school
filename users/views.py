0f56d045219c0f153b6b1bdf4ced9374b42b48fe
vasilij korobov <kavyabass51@gmail.com>
2025-05-08 01:19:42 +0700

    Задание 4

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+
+from users.filters import PaymentFilter
 from users.models import Payment
 from users.serializers import PaymentSerializer
 
@@ -6,3 +9,5 @@ from users.serializers import PaymentSerializer
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PaymentFilter
