33167d2413220e2e589a22d981c7bb6cbb907bdf
vasilij korobov <kavyabass51@gmail.com>
2025-05-14 01:26:51 +0700

    Задание 2

--- materials/views.py
+++ materials/views.py
@@ -1,13 +1,22 @@
 from django.db import models
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.annotate(
         lessons_count=models.Count('lessons')).prefetch_related('lessons')
     serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            self.permission_classes = [IsAdminUser]
+        elif self.action in ['update', 'partial_update', 'retrieve', 'list']:
+            self.permission_classes = [IsAuthenticated | IsModerator]
+        return [permission() for permission in self.permission_classes]
 
 
 class LessonListCreateView(generics.ListCreateAPIView):
@@ -18,3 +27,20 @@ class LessonListCreateView(generics.ListCreateAPIView):
 class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            self.permission_classes = [IsAdminUser]
+        elif self.action in ['update', 'partial_update', 'retrieve', 'list']:
+            self.permission_classes = [IsAuthenticated | IsModerator]
+        return [permission() for permission in self.permission_classes]
+
+    def get_queryset(self):
+        if not self.request.user.groups.filter(name='moderators').exists():
+            return Lesson.objects.filter(owner=self.request.user)
+        return Lesson.objects.all()
--- users/permissions.py
+++ users/permissions.py
@@ -4,3 +4,8 @@ from rest_framework import permissions
 class IsAdminOrOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or obj == request.user
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='moderators').exists()
